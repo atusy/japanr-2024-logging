06e487686d891f3cfab0e2e13c5cdf8ebad79f45
atusy <30277794+atusy@users.noreply.github.com>
2024-11-18 09:37:06 +0900

    feat(logger): add structured logs

--- R/app.R
+++ R/app.R
@@ -1,28 +1,72 @@
-plot_weather_forecast <- function(x) {
+logger::log_formatter(logger::formatter_json)
+logger::log_layout(
+  logger::layout_json_parser(fields = c("time", "level", "ns"))
+)
+
+log <- function(level, message, ctx = list()) {
+  purrr::exec(logger::log_level, level, message = message, !!!ctx)
+}
+
+plot_weather_forecast <- function(x, ctx = list()) {
+  log(logger::DEBUG, "Plotting weather forecast", ctx)
   .data <- rlang::.data
 
-  location <- openmeteo::geocode(x)
-
-  forecast <- openmeteo::weather_forecast(
-    c(location$latitude, location$longitude),
-    hourly = "temperature_2m"
+  geocode <- openmeteo::geocode(x)
+  log(logger::DEBUG,
+    "Resolved geocode",
+    c(ctx, list(geocode = geocode))
   )
 
-  ggplot2::ggplot(forecast) +
+  forecast <- openmeteo::weather_forecast(
+    c(geocode$latitude, geocode$longitude),
+    hourly = "temperature_2m"
+  )
+  log(logger::DEBUG, "Forecasted weather", ctx)
+
+  p <- ggplot2::ggplot(forecast) +
     ggplot2::aes(.data$datetime, .data$hourly_temperature_2m) +
     ggplot2::geom_line() +
     ggplot2::labs(
-      title = location$name,
+      title = geocode$name,
       subtitle = paste(
         "Latitude:",
-        location$latitude,
+        geocode$latitude,
         "Longitude:",
-        location$longitude
+        geocode$longitude
       )
     )
+  log(logger::DEBUG, "Forecasted weather", ctx)
+
+  return(p)
 }
 
 server <- function(input, output, session) {
+  logger::log_threshold(logger::DEBUG)
+
+  # Log session start
+  session_id <- RcppUUID::uuid_generate_random()
+  logger::log_info(message = "Session started", session_id = session_id)
+
+  # Configure logger
+  trace_id <- shiny::reactiveVal(NA_character_) # NA behaves as null in JSON
+  ctx <- shiny::reactive({
+    list(
+      session_id = session_id,
+      trace_id = trace_id()
+    )
+  })
+
+  # Record request ID and optionally log request parameters
+  shiny::observe({
+    params <- shiny::reactiveValuesToList(input)
+    trace_id(RcppUUID::uuid_generate_random())
+    log(logger::DEBUG,
+      "Request received",
+      c(list(request_params = params), ctx())
+    )
+  })
+
+  # Async strategy
   oldplan <- future::plan()
   on.exit(future::plan(oldplan))
   if (.Platform$OS.type == "windows") {
@@ -37,11 +81,18 @@ server <- function(input, output, session) {
     )
   }
 
+  # Parse locations as comma-separated values
   locations <- shiny::reactive({
     shiny::req(input$locations)
-    input$locations |>
+    ret <- input$locations |>
       stringi::stri_split_regex("\\s*,\\s*") |>
       unlist()
+    log(
+      logger::DEBUG,
+      "Locations parsed",
+      c(list(value = ret), ctx())
+    )
+    ret
   })
 
   output$weather <- shiny::renderUI({
@@ -53,16 +104,24 @@ server <- function(input, output, session) {
 
   shiny::observe({
     lapply(locations(), function(location) {
+      ctx_span <- c(
+        list(location = location, span_id = RcppUUID::uuid_generate_random()),
+        ctx()
+      )
+      log(logger::DEBUG, "Processing location", ctx_span)
       plot_id <- paste0("weather_", location)
       future::future({
-        plot_weather_forecast(location)
+        plot_weather_forecast(location, ctx_span)
       }) |>
         promises::then(function(x) {
-          output[[plot_id]] <- shiny::renderPlot(
-            {
-              x
-            },
-            res = 100
+          output[[plot_id]] <- shiny::renderPlot(x, res = 100)
+          log(logger::DEBUG, "Processed location", ctx_span)
+        }) |>
+        promises::catch(function(e) {
+          log(
+            logger::ERROR,
+            "Failed to process location",
+            c(list(error = as.list(format(e))), ctx_span)
           )
         })
     })
@@ -77,5 +136,5 @@ ui <- function() {
 }
 
 run <- function() {
-  shiny::shinyApp(ui(), server)
+  shiny::shinyApp(ui(), server, option = list(port = 5604))
 }
