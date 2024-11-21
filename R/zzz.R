1baaa0ed42ea04af4805f323172d1437d2749d05
atusy <30277794+atusy@users.noreply.github.com>
2024-11-21 09:55:31 +0900

    fix: logger config requires explicit namespace

--- R/zzz.R
+++ R/zzz.R
@@ -1,20 +1,37 @@
 .onLoad <- function(libname, pkgname) {
-  logger::log_threshold(`::`("logger", Sys.getenv("JAPANR2024_LOG_LEVEL", "INFO")))
-  log_appender <- stringr::str_split(Sys.getenv("JAPANR2024_LOG_APPENDER"), ";")[[1]]
+  ns_logger <- asNamespace("logger")
+  ns <- get("top_env_name", ns_logger)
+
+  # log level
+  logger::log_threshold(
+    get(
+      Sys.getenv("JAPANR2024_LOG_LEVEL", "INFO"),
+      envir = ns_logger
+    ),
+    ns
+  )
+
+  # log to stdout and/or file
+  log_appender <- stringr::str_split(
+    Sys.getenv("JAPANR2024_LOG_APPENDER"),
+    ";"
+  )[[1]]
   if (!identical(log_appender, "")) {
     stdout <- "-" %in% log_appender
     file <- log_appender[!stdout]
     if (any(stdout)) {
       if (length(file) > 0) {
-        logger::log_appender(logger::appender_tee(file))
+        logger::log_appender(logger::appender_tee(file), ns)
       }
     } else if (length(file) > 0) {
-      logger::log_appender(logger::appender_file(file))
+      logger::log_appender(logger::appender_file(file), ns)
     }
   }
-  logger::log_appender(logger::appender_stdout)
-  logger::log_formatter(logger::formatter_json)
+
+  # log format
+  logger::log_formatter(logger::formatter_json, ns)
   logger::log_layout(
-    logger::layout_json_parser(fields = c("time", "level", "ns"))
+    logger::layout_json_parser(fields = c("time", "level", "ns")),
+    ns
   )
 }
