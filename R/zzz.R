2e0a1cf7edce805aefdcfd7345d7e9b4831c5f32
atusy <30277794+atusy@users.noreply.github.com>
2024-11-21 09:16:30 +0900

    feat(logger): tweak with envvars

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,20 @@
 .onLoad <- function(libname, pkgname) {
-  logger::log_appender(logger::appender_tee("__ignored/japanr2024.log"))
+  logger::log_threshold(`::`("logger", Sys.getenv("JAPANR2024_LOG_LEVEL", "INFO")))
+  log_appender <- stringr::str_split(Sys.getenv("JAPANR2024_LOG_APPENDER"), ";")[[1]]
+  if (!identical(log_appender, "")) {
+    stdout <- "-" %in% log_appender
+    file <- log_appender[!stdout]
+    if (any(stdout)) {
+      if (length(file) > 0) {
+        logger::log_appender(logger::appender_tee(file))
+      }
+    } else if (length(file) > 0) {
+      logger::log_appender(logger::appender_file(file))
+    }
+  }
+  logger::log_appender(logger::appender_stdout)
   logger::log_formatter(logger::formatter_json)
   logger::log_layout(
     logger::layout_json_parser(fields = c("time", "level", "ns"))
   )
-  logger::log_threshold(logger::DEBUG)
 }
