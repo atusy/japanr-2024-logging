eb7286a36cb4f4c21477e131e747f98552babb99
atusy <30277794+atusy@users.noreply.github.com>
2024-11-26 11:03:53 +0900

    fix: log not appended to file

--- R/zzz.R
+++ R/zzz.R
@@ -2,13 +2,20 @@
   # log level
   logger::log_threshold(Sys.getenv("JAPANR2024_LOG_LEVEL", "OFF"), pkgname)
 
+  # log format
+  logger::log_formatter(logger::formatter_json, pkgname)
+  logger::log_layout(
+    logger::layout_json_parser(fields = c("time", "level", "ns")),
+    pkgname
+  )
+
   # log to stdout and/or file
   log_appender <- stringr::str_split(
     Sys.getenv("JAPANR2024_LOG_APPENDER"),
     ";"
   )[[1]]
   if (!identical(log_appender, "")) {
-    stdout <- "-" %in% log_appender
+    stdout <- log_appender == "-"
     file <- log_appender[!stdout]
     if (any(stdout)) {
       if (length(file) > 0) {
@@ -18,11 +25,4 @@
       logger::log_appender(logger::appender_file(file), pkgname)
     }
   }
-
-  # log format
-  logger::log_formatter(logger::formatter_json, pkgname)
-  logger::log_layout(
-    logger::layout_json_parser(fields = c("time", "level", "ns")),
-    pkgname
-  )
 }
