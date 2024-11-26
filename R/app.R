f577ad18a43349bf29b7686456715a2556a4fa9a
atusy <30277794+atusy@users.noreply.github.com>
2024-11-26 10:44:02 +0900

    feat: allow tweaking logger and future strategy

--- R/app.R
+++ R/app.R
@@ -1,8 +1,8 @@
-log <- function(level, message, ctx = list()) {
+.log <- function(level, message, ctx = list()) {
   purrr::exec(logger::log_level, level, message = message, !!!ctx)
 }
 
-plot_weather_forecast <- function(x, ctx = list()) {
+plot_weather_forecast <- function(x, log = .log, ctx = list()) {
   log(logger::DEBUG, "Plotting weather forecast", ctx)
   .data <- rlang::.data
 
@@ -35,92 +35,105 @@ plot_weather_forecast <- function(x, ctx = list()) {
   return(p)
 }
 
-server <- function(input, output, session) {
-  logger::log_threshold(logger::DEBUG)
-
-  # Log session start
-  session_id <- ulid::ulid()
-  logger::log_info(message = "Session started", session_id = session_id)
-
-  # Configure logger
-  trace_id <- shiny::reactiveVal(NA_character_) # NA behaves as null in JSON
-  ctx <- shiny::reactive({
-    list(
-      session_id = session_id,
-      trace_id = trace_id()
-    )
-  })
-
-  # Record request ID and optionally log request parameters
-  shiny::observe({
-    params <- shiny::reactiveValuesToList(input)
-    trace_id(ulid::ulid())
-    log(logger::DEBUG,
-      "Request received",
-      c(list(request_params = params), ctx())
-    )
-  })
-
+server <- function(plan = NULL, log = .log) {
+  force(log)
   # Async strategy
   oldplan <- future::plan()
-  on.exit(future::plan(oldplan))
-  if (.Platform$OS.type == "windows") {
-    future::plan(
-      future::multisession,
-      workers = min(5L, future::availableCores())
-    )
-  } else {
-    future::plan(
-      future::multicore,
-      workers = min(5L, future::availableCores(constraints = "multicore"))
-    )
+  apply_plan <- function() {
+    if (!is.null(plan)) {
+      future::plan(plan)
+    } else if (.Platform$OS.type == "windows") {
+      future::plan(
+        future::multisession,
+        workers = min(5L, future::availableCores())
+      )
+    } else {
+      future::plan(
+        future::multicore,
+        workers = min(5L, future::availableCores(constraints = "multicore"))
+      )
+    }
   }
 
-  # Parse locations as comma-separated values
-  locations <- shiny::reactive({
-    shiny::req(input$locations)
-    ret <- input$locations |>
-      stringi::stri_split_regex("\\s*,\\s*") |>
-      unlist()
-    log(
-      logger::DEBUG,
-      "Locations parsed",
-      c(list(value = ret), ctx())
-    )
-    ret
-  })
+  # Server function
+  function(input, output, session) {
+    # Setup async strategy
+    on.exit(future::plan(oldplan))
+    apply_plan()
 
-  output$weather <- shiny::renderUI({
-    locations() |>
-      lapply(function(x) {
-        shiny::plotOutput(outputId = paste0("weather_", x))
-      })
-  })
+    # Log session start
+    session_id <- ulid::ulid()
+    log(logger::INFO, message = "Session started", list(session_id = session_id))
 
-  shiny::observe({
-    lapply(locations(), function(location) {
-      ctx_span <- c(
-        list(location = location, span_id = ulid::ulid()),
-        ctx()
+    # Configure logger
+    trace_id <- shiny::reactiveVal(NA_character_) # NA behaves as null in JSON
+    ctx <- shiny::reactive({
+      list(session_id = session_id, trace_id = trace_id())
+    })
+
+    # Record request ID and optionally log request parameters
+    shiny::observe({
+      params <- shiny::reactiveValuesToList(input)
+      trace_id(ulid::ulid())
+      log(logger::DEBUG,
+        "Request received",
+        c(list(request_params = params), ctx())
       )
-      log(logger::DEBUG, "Processing location", ctx_span)
-      plot_id <- paste0("weather_", location)
-      future::future({
-        plot_weather_forecast(location, ctx_span)
-      }) |>
-        promises::then(function(x) {
-          output[[plot_id]] <- shiny::renderPlot(x, res = 100)
-          log(logger::DEBUG, "Processed location", ctx_span)
-        }) |>
-        promises::catch(function(e) {
-          log(
-            logger::ERROR,
-            "Failed to process location",
-            c(list(error = as.list(format(e))), ctx_span)
-          )
+    })
+
+    # Parse locations as comma-separated values
+    locations <- shiny::reactive({
+      shiny::req(input$locations)
+      ret <- input$locations |>
+        stringi::stri_split_regex("\\s*,\\s*") |>
+        unlist()
+      log(
+        logger::DEBUG,
+        "Locations parsed",
+        c(list(value = ret), ctx())
+      )
+      ret
+    })
+
+    # Prepare desired number of weather forecast plots
+    output$weather <- shiny::renderUI({
+      locations() |>
+        lapply(function(x) {
+          shiny::plotOutput(outputId = paste0("weather_", x))
         })
     })
-  })
+
+    # Draw weather forecast plots
+    shiny::observe({
+      lapply(locations(), function(location) {
+        # task-specific logging context
+        ctx_span <- c(
+          list(location = location, span_id = ulid::ulid()),
+          ctx()
+        )
+
+        log(logger::DEBUG, "Processing location", ctx_span)
+
+        plot_id <- paste0("weather_", location)
+        future::future({
+          plot_weather_forecast(location, log, ctx_span)
+        }) |>
+          promises::then(function(x) {
+            output[[plot_id]] <- shiny::renderPlot(x, res = 100)
+            log(logger::DEBUG, "Processed location", ctx_span)
+          }) |>
+          promises::catch(function(e) {
+            log(
+              logger::ERROR,
+              "Failed to process location",
+              c(list(error = as.list(format(e))), ctx_span)
+            )
+          })
+
+        # Do not log "Processed location" here as it is async
+      })
+    })
+  }
 }
 
 ui <- function() {
@@ -130,6 +143,10 @@ ui <- function() {
   )
 }
 
-run <- function() {
-  shiny::shinyApp(ui(), server, option = list(port = 5604))
+run <- function(plan = NULL, log = NULL) {
+  shiny::shinyApp(
+    ui(),
+    server(plan = plan, log = .log),
+    option = list(port = 5604)
+  )
 }
